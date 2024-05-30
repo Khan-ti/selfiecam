cafa8ef7dd3904d9ad36d7054547363620f061ef
Khan-ti <75572484+Khan-ti@users.noreply.github.com>
2024-05-30 13:33:03 +0630

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import "./style.css";
-import { getVideo, drawVideo } from "./camera";
+import { getVideo, drawVideo } from "./src/camera";
 
 (async function run() {
   const video = await getVideo();
